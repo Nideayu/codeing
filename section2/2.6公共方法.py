94c22ca2863fe94515f7dc7d7db941ff938331d7
Nideayu <1242469403@qq.com>
2020-12-10 19:57:03 +0800

    section2

---
+++ section2/2.6公共方法.py
@@ -0,0 +1,14 @@
+'''
+
+python内置函数
+    Python包含了以下内置函数
+
+    序号	方法	描述
+    1	cmp(item1, item2)	比较两个值
+    2	len(item)	计算容器中元素个数
+    3	max(item)	返回容器中元素最大值
+    4	min(item)	返回容器中元素最小值
+    5	del(item)	删除变量
+    '''
+# a = cmp(1,2)
+# print(a)
---
+++ section2/作业 .py
@@ -0,0 +1,16 @@
+# 1. 编程实现对一个元素全为数字的列表，求最大值、最小值
+# list = [1,2,3,4,5,6,7,8,9]
+# # 最大值
+# liasts = max(list)
+# print(liasts)
+# # 最小值
+# lists = min(list)
+# print(lists)
+
+
+# 2.编写程序，完成以下要求：
+# 统计字符串中，各个字符的个数
+# 比如："hello world" 字符串统计的结果为： h:1 e:1 l:3 o:2 d:1 r:1 w:1
+astr = 'hello world'
+a = astr.count('h')
+print(a)
